40fa9afcadbdf549404647ea6d83c746bc2dea73
Kushal797-bit <kushalupadhyay06@example.com>
2026-07-14 23:37:04 +0530

    Load sentence transformer lazily

--- ml/match_score.py
+++ ml/match_score.py
@@ -1,13 +1,22 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load model once
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# Don't load the model immediately
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model
+
 
 def get_match_score(resume_text, jd_text):
     """
     Returns similarity score between resume and job description.
     """
+
+    model = get_model()
 
     embeddings = model.encode([resume_text, jd_text])
 
@@ -16,6 +25,4 @@ def get_match_score(resume_text, jd_text):
         [embeddings[1]]
     )[0][0]
 
-    match_percent = round(float(score) * 100, 2)
-
-    return match_percent
+    return round(float(score) * 100, 2)
